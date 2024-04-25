a933b2450b613a4030af5cc5bbff9b2969297f5f
Aulianurfitria15 <aulianurfitria17@gmail.com>
2024-04-25 17:50:22 +0700

    void traverse (else)

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -101,4 +101,12 @@ void traverse() {
 		return;
 	}
 
-	
+	else {
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << "NIM:" << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
+
